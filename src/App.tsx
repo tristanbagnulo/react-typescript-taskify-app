cee583213ac7e40db91da9476cccc5ec54275d91
Tristan Bagnulo <tristan.bagnulo1@gmail.com>
2022-10-08 18:21:47 +1100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './model';
+
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  //add from 32:40 to 37:10 https://www.youtube.com/watch?v=FJDVKeh7RJI
+
+  console.log(todo);
+  
+
+  return(
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo}/>
+
+    </div>
+  );
+}
+
+
+export default App;
